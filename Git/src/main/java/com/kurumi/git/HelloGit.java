5cf01909b9f35a6eb9f31e6be5e10139b609a0a6
kurumiwj <zhuwenjienet@126.com>
2023-03-23 21:40:20 +0800

    SSH测试

--- Git/src/main/java/com/kurumi/git/HelloGit.java
+++ Git/src/main/java/com/kurumi/git/HelloGit.java
@@ -7,5 +7,6 @@ public class HelloGit {
         System.out.println("创建分支！");
         System.out.println("这是主干！");
         System.out.println("这是分支！");
+        System.out.println("ssh测试！");
     }
 }
